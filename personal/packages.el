6506aeb58b591ec2d24518ac9123bb6fc69d989c
Stefan <stk@idagio.com>
2019-02-26 13:56:24 +0100

    Latest changes

--- personal/packages.el
+++ personal/packages.el
@@ -4,9 +4,12 @@
     projectile
     py-autopep8
     exec-path-from-shell
+    multiple-cursors
     material-theme))
 
 (mapc #'(lambda (package)
     (unless (package-installed-p package)
       (package-install package)))
       myPackages)
+
+(require 'multiple-cursors)
