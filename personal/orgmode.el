dd423a088bd9fd946aaa77469a417d6f81fc77c0
Stefan <stk@idagio.com>
2019-03-01 17:53:14 +0100

    Lets get started with orgmode

---
+++ personal/orgmode.el
@@ -0,0 +1,6 @@
+(require 'org)
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+
+(setq org-agenda-files (list "~/org/idagio.org"
+                             "~/org/stefan.org"
+                             "~/org/notes.org"))
