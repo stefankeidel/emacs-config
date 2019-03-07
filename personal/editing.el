a44c3f6877742b23da658a1deeb3c4ad893d41b9
Stefan <stk@idagio.com>
2019-03-07 18:16:55 +0100

    Remove weird SQL indentation hack. This needs more work

--- personal/editing.el
+++ personal/editing.el
@@ -6,27 +6,3 @@
 (setq prelude-guru nil)
 (setq-default create-lockfiles nil)
 (setq-default whitespace-line-column 120)
-
-
-(defun my-indent ()
-  (let ((last-indent (if (> (line-number-at-pos) 1)
-                         (save-excursion
-                           (previous-line)
-                           (back-to-indentation)
-                           (current-column))
-                       0)))
-    (save-excursion
-      (back-to-indentation)
-      (if (and (eq last-command this-command)
-               (> (point) (line-beginning-position)))
-          (delete-region (max (line-beginning-position) (- (point) 4)) (point))
-        (while (< (current-column) (+ 4 last-indent))
-          (insert " "))))
-    (if (< (point) (save-excursion (back-to-indentation) (point)))
-        (back-to-indentation))))
-
-
-(defun my-sql-mode-hook ()
-  (setq indent-line-function 'my-indent))
-
-(add-hook 'sql-mode-hook 'my-sql-mode-hook)
