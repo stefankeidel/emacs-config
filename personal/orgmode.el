e08277a26839a04b9828e9fca187e31908371379
Stefan <stk@idagio.com>
2019-03-11 15:04:25 +0100

    orgmode improvements

--- personal/orgmode.el
+++ personal/orgmode.el
@@ -1,9 +1,22 @@
 (require 'org)
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 
-(setq org-agenda-files (list "~/org/idagio.org"
-                             "~/org/stefan.org"
+(setq org-agenda-files (list "~/org/todo.org"
                              "~/org/notes.org"
                              "~/org/birthdays.org"))
+
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "MEETING"))))
+
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "red" :weight bold)
+              ("NEXT" :foreground "SkyBlue" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold)
+              ("WAITING" :foreground "orange" :weight bold)
+              ("HOLD" :foreground "magenta" :weight bold)
+              ("CANCELLED" :foreground "forest green" :weight bold)
+              ("MEETING" :foreground "forest green" :weight bold)
+              ("PHONE" :foreground "forest green" :weight bold))))
 
 (org-agenda-list)
