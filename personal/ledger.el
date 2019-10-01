b801b4e0e23c09224dc47a197d9420e4f92c6c0e
Stefan <1188614+stefankeidel@users.noreply.github.com>
2019-10-01 11:48:28 +0200

    This is how you define custom reports

--- personal/ledger.el
+++ personal/ledger.el
@@ -1,1 +1,10 @@
 (setq ledger-reconcile-default-commodity "EUR")
+
+(setq ledger-reports
+      (quote
+       (("register" "ledger ")
+        ("bal" "ledger -f %(ledger-file) --price-db ./prices.db -V bal Assets Liabilities")
+        ("bigbal" "ledger -f %(ledger-file) bal Assets Liabilities")
+        ("reg" "ledger -f %(ledger-file) reg")
+        ("payee" "ledger -f %(ledger-file) reg @%(payee)")
+        ("account" "ledger -f %(ledger-file) reg %(account)"))))
