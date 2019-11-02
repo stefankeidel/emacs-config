c45a1ca9b37f73eb4873d252dcc8cb9a4b2cafd1
Stefan <1188614+stefankeidel@users.noreply.github.com>
2019-11-02 16:51:05 +0100

    Have reports show empty stuff

--- personal/ledger.el
+++ personal/ledger.el
@@ -3,9 +3,9 @@
 (setq ledger-reports
       (quote
        (("baldaily" "ledger -f %(ledger-file) bal '^assets:comdirect|^assets:gls|^liabilities' --empty")
-        ("totalbudget" "ledger --monthly -S -T -f %(ledger-file) bal ^assets:budget")
-        ("lastmonthbudget" "ledger --monthly -S -T -f %(ledger-file) bal ^assets:budget --period 'last month'")
-        ("currentbudget" "ledger --monthly -S -T -f %(ledger-file) bal ^assets:budget --period 'this month'")
+        ("totalbudget" "ledger --monthly -S -T -f %(ledger-file) bal ^assets:budget --empty")
+        ("lastmonthbudget" "ledger --monthly -S -T -f %(ledger-file) bal ^assets:budget --period 'last month' --empty")
+        ("currentbudget" "ledger --monthly -S -T -f %(ledger-file) bal ^assets:budget --period 'this month' --empty")
         ("networth" "ledger --real -V --price-db market.db -f %(ledger-file) bal '^assets|^liabilities'")
         ("networthovertime" "ledger --real --market --price-db market.db -f %(ledger-file) reg '^assets|^liabilities' --collapse --monthly -O")
         )))
