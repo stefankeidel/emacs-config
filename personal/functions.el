e733350a50984fee374c866abe2c89c76f472ac2
Stefan <1188614+stefankeidel@users.noreply.github.com>
2019-11-14 10:14:02 +0100

    Update

--- personal/functions.el
+++ personal/functions.el
@@ -2,30 +2,35 @@
   "Run dbt test"
   (interactive)
 
-  (let ((default-directory "~/Documents/idagio/idagio-analytics-scripts/dbt/"))
+  (let ((default-directory "~/Documents/idagio/idagio-dbt/"))
     (async-shell-command "dbt test")))
 
 (defun idagio-dbt-run ()
   "Run dbt"
   (interactive)
 
-  (let ((default-directory "~/Documents/idagio/idagio-analytics-scripts/dbt/"))
+  (let ((default-directory "~/Documents/idagio/idagio-dbt/"))
     (async-shell-command "dbt run")))
 
 (defun idagio-dbt-run-models-interactive (model)
   "Run dbt with model selection entered by user"
   (interactive "sEnter model selection:")
 
-  (let ((default-directory "~/Documents/idagio/idagio-analytics-scripts/dbt/"))
+  (let ((default-directory "~/Documents/idagio/idagio-dbt/"))
     (async-shell-command
      (format "dbt run --models %s"
              (shell-quote-argument model)))))
 
-(defun idagio-dbt-run-models-dag-current-buffer ()
-  "Run dbt with model selection entered by user"
+(defun idagio-dbt-run-models-downstream ()
+  "Run dbt with everything downstream from the current buffer (including the current one)"
   (interactive)
 
-  (let ((default-directory "~/Documents/idagio/idagio-analytics-scripts/dbt/"))
+  (let ((default-directory "~/Documents/idagio/idagio-dbt/"))
     (async-shell-command
-     (format "dbt run --models +%s+"
+     (format "dbt run --models %s+"
              (shell-quote-argument (file-name-base (buffer-file-name)))))))
+
+(defun kill-buffer-basename ()
+  "Kill buffer basename"
+  (interactive)
+  (kill-new (file-name-base (buffer-file-name))))
