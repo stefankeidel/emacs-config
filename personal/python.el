2dc29e67373231b0b69ebcc33e8c26fd14847f29
Stefan <1188614+stefankeidel@users.noreply.github.com>
2020-02-12 18:05:23 +0100

    Moved to /opt

--- personal/python.el
+++ personal/python.el
@@ -1,7 +1,7 @@
 ;; elpy
 (elpy-enable)
 (setq elpy-rpc-backend "jedi")
-(setq elpy-rpc-python-command "/Users/stefan/anaconda3/bin/python")
+(setq elpy-rpc-python-command "/opt/anaconda3/bin/python")
 
-(setq python-shell-interpreter "/Users/stefan/anaconda3/bin/ipython"
+(setq python-shell-interpreter "/opt/anaconda3/bin/ipython"
       python-shell-interpreter-args "-i --simple-prompt")
