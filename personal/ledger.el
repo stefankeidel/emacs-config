d8593562939dd0bd47fb11ac357be4c92488b3e6
Stefan <1188614+stefankeidel@users.noreply.github.com>
2022-10-08 13:18:01 +0200

    +hledger

--- personal/ledger.el
+++ personal/ledger.el
@@ -9,3 +9,24 @@
         ("networth" "ledger --real -V --price-db market.db -f %(ledger-file) bal '^assets|^liabilities'")
         ("overtime" "ledger --real --market --price-db market.db -f %(ledger-file) reg '^assets|^liabilities' --collapse --monthly -O")
         )))
+
+
+;;; Basic configuration
+(require 'hledger-mode)
+
+;; To open files with .journal extension in hledger-mode
+(add-to-list 'auto-mode-alist '("\\.ledger\\'" . hledger-mode))
+
+;; Provide the path to you journal file.
+;; The default location is too opinionated.
+(setq hledger-jfile "~/Cloud/ledger/stefan.ledger")
+
+
+;;; Auto-completion for account names
+;; For company-mode users,
+(add-to-list 'company-backends 'hledger-company)
+
+
+                                        ; for reporting
+
+(setq hledger-ratios-liquid-asset-accounts "assets:gls")
