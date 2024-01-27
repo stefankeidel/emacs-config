492f20617dc30a1e516db5bb1b786ed495f55a0f
Stefan <1188614+stefankeidel@users.noreply.github.com>
2024-01-27 16:00:31 +0100

    Some cleanup

--- personal/functions.el
+++ personal/functions.el
@@ -1,72 +1,72 @@
-(exec-path-from-shell-copy-env "DBT_PROFILES_DIR")
-(exec-path-from-shell-copy-env "DBT_DEV_SCHEMA")
-(exec-path-from-shell-copy-env "LICHTBLICK_RAW_DATABASE_NAME")
+;(exec-path-from-shell-copy-env "DBT_PROFILES_DIR")
+;(exec-path-from-shell-copy-env "DBT_DEV_SCHEMA")
+;(exec-path-from-shell-copy-env "LICHTBLICK_RAW_DATABASE_NAME")
 
-(defun lichtblick-dbt-test ()
-  "Run dbt test"
-  (interactive)
+;; (defun lichtblick-dbt-test ()
+;;   "Run dbt test"
+;;   (interactive)
 
-  (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
-    (async-shell-command "dbt test")))
+;;   (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
+;;     (async-shell-command "dbt test")))
 
-(defun lichtblick-dbt-run ()
-  "Run dbt"
-  (interactive)
+;; (defun lichtblick-dbt-run ()
+;;   "Run dbt"
+;;   (interactive)
 
-  (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
-    (async-shell-command "dbt run")))
+;;   (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
+;;     (async-shell-command "dbt run")))
 
-(defun lichtblick-dbt-clean-compile ()
-  "Clean+compile dbt"
-  (interactive)
+;; (defun lichtblick-dbt-clean-compile ()
+;;   "Clean+compile dbt"
+;;   (interactive)
 
-  (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
-    (async-shell-command "dbt clean && dbt deps && dbt compile")))
+;;   (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
+;;     (async-shell-command "dbt clean && dbt deps && dbt compile")))
 
-(defun lichtblick-dbt-run-models-interactive (model)
-  "Run dbt with model selection entered by user"
-  (interactive "sEnter model selection:")
+;; (defun lichtblick-dbt-run-models-interactive (model)
+;;   "Run dbt with model selection entered by user"
+;;   (interactive "sEnter model selection:")
 
-  (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
-    (async-shell-command
-     (format "dbt run --models %s"
-             (shell-quote-argument model)))))
+;;   (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
+;;     (async-shell-command
+;;      (format "dbt run --models %s"
+;;              (shell-quote-argument model)))))
 
-(defun lichtblick-dbt-run-models-downstream ()
-  "Run dbt with everything downstream from the current buffer (including the current one)"
-  (interactive)
+;; (defun lichtblick-dbt-run-models-downstream ()
+;;   "Run dbt with everything downstream from the current buffer (including the current one)"
+;;   (interactive)
 
-  (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
-    (async-shell-command
-     (format "dbt run --models %s+"
-             (shell-quote-argument (file-name-base (buffer-file-name)))))))
+;;   (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
+;;     (async-shell-command
+;;      (format "dbt run --models %s+"
+;;              (shell-quote-argument (file-name-base (buffer-file-name)))))))
 
-(defun lichtblick-dbt-run-model ()
-  "Run dbt with model from the current buffer"
-  (interactive)
+;; (defun lichtblick-dbt-run-model ()
+;;   "Run dbt with model from the current buffer"
+;;   (interactive)
 
-  (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
-    (async-shell-command
-     (format "dbt run --models %s"
-             (shell-quote-argument (file-name-base (buffer-file-name)))))))
+;;   (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
+;;     (async-shell-command
+;;      (format "dbt run --models %s"
+;;              (shell-quote-argument (file-name-base (buffer-file-name)))))))
 
-(defun lichtblick-dbt-run-models-lineage ()
-  "Run dbt with the entire lineage from the current buffer (including the current one)"
-  (interactive)
+;; (defun lichtblick-dbt-run-models-lineage ()
+;;   "Run dbt with the entire lineage from the current buffer (including the current one)"
+;;   (interactive)
 
-  (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
-    (async-shell-command
-     (format "dbt run --models +%s+"
-             (shell-quote-argument (file-name-base (buffer-file-name)))))))
+;;   (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
+;;     (async-shell-command
+;;      (format "dbt run --models +%s+"
+;;              (shell-quote-argument (file-name-base (buffer-file-name)))))))
 
-(defun lichtblick-dbt-test-model ()
-  "Run all model tests for model defined by current buffer"
-  (interactive)
+;; (defun lichtblick-dbt-test-model ()
+;;   "Run all model tests for model defined by current buffer"
+;;   (interactive)
 
-  (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
-    (async-shell-command
-     (format "dbt test --models %s"
-             (shell-quote-argument (file-name-base (buffer-file-name)))))))
+;;   (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
+;;     (async-shell-command
+;;      (format "dbt test --models %s"
+;;              (shell-quote-argument (file-name-base (buffer-file-name)))))))
 
 (defun kill-buffer-basename ()
   "Kill buffer basename"
@@ -89,42 +89,10 @@
    "~/code/lichtblick/cloud_dwh/dbt/target/compiled/lb_dwh/"
    (concat (file-name-base (buffer-file-name)) ".sql")))
 
-;; (defun lichtblick-dbt-compile-production ()
-;;   "Compile dbt for production"
-;;   (interactive)
-
-;;   (let ((default-directory "~/code/lichtblick/cloud_dwh/dbt/"))
-;;     (async-shell-command "dbt compile --target prod")))
-
-;; (defun lichtblick-export-query-results-csv ()
-;;   "Export results from query as defined by current file"
-;;   (interactive)
-
-;;   (make-temp-file "idagio-export-csv")
-;;   (let ((default-directory "~/Documents/idagio/idagio-analytics-scripts/")
-;;         (tmpf (make-temp-file "idagio-export-csv")))
-;;     (shell-command
-;;      (format "python utils/sql2csv.py --query %s --outfile %s"
-;;              (shell-quote-argument (buffer-file-name))
-;;              tmpf))
-;;     (find-file-other-window tmpf)))
-
-(defun stefan-schreibt-deutsch ()
-  (interactive)
-  (setq ns-alternate-modifier 'none ns-right-alternate-modifier 'meta)
-  )
-
-(defun stefan-braucht-meta-key ()
-  (interactive)
-  (setq ns-alternate-modifier 'meta ns-right-alternate-modifier 'none)
-  )
-
-(defun snowflake-select-field-list (number)
-  (interactive "nHow many fields:")
-  (insert "t.$")
-  (insert (mapconcat 'number-to-string (number-sequence 1 number) ",t.$"))
-  )
-
 (defun connect-new-srv ()
   (interactive)
-  (find-file "/ssh:stefan@cloud.keidel.me#4321:/home/stefan"))
+  (find-file "/ssh:stefan@janeway#4321:/home/stefan"))
+
+(defun connect-rpi ()
+  (interactive)
+  (find-file "/ssh:stefan@raspberrypi:/home/stefan"))
