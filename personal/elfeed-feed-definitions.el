9d232c2e9cd0aefb915c982c643e22fcb319bb7c
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2024-02-08 09:16:40 +0100

    feed stuff

--- personal/elfeed-feed-definitions.el
+++ personal/elfeed-feed-definitions.el
@@ -39,29 +39,23 @@
                                         ; useful geek stuff
         ("http://nullprogram.com/feed/" programming)
         ("https://hnrss.org/frontpage" hn maybe) ; spammy custom tag
-        ("http://n-gate.com/index.rss" blog)
-        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCUHW94eEFW7hkUMVaZz4eDg" physics interesting) ; minutephysics
-        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCoxcjq-8xIDTYp3uz647V5A" interesting) ; numberphile
         ("https://datawhatnow.com/feed" blog interesting)
-        ("https://plaintextproject.online/feed.xml" blog interesting)
 
         ("https://events.ccc.de/feed/" events security)
         ("https://www.ccc.de/rss/updates.rdf" security)
 
-        ("https://protonmail.com/blog/feed/" security interesting)
-        ("https://www.benkuhn.net/rss/" blog)
+        ("https://www.benkuhn.net/index.xml" blog)
         ("https://metaredux.com/feed.xml" blog)
         ("https://github.blog/feed/atom" blog)
         ;("http://www.aaronsw.com/2002/feeds/pgessays.rss" blog)
         ;("http://feeds.feedburner.com/marginalrevolution/feed" blog)
-        ;("https://paulskallas.substack.com/feed" blog) ; 4HL dude
         ("http://feeds.feedburner.com/martinkl" blog)
         ("https://www.lesswrong.com/feed.xml?view=curated-rss" blog)
 
                                         ; emacs
         ("https://asylum.madhouse-project.org/blog/atom.xml" emacs blog)
         ("http://www.masteringemacs.org/feed/" emacs blog)
-        ("http://pragmaticemacs.com/feed/" emacs blog)
+        ("https://pragmaticemacs.wordpress.com/feed" emacs blog)
         ("https://emacsredux.com/atom.xml" emacs blog)
                                         ;("https://sachachua.com/blog/feed/" emacs blog)
         ("https://feedpress.me/TheTechnium" blog)
@@ -76,9 +70,11 @@
                                         ;environment
         ;("https://www.gatesnotes.com/RSS" environment blog)
 
-                                        ; bike stuff
+                                        ; yt
         ;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UC710HJmp-YgNbE5BnFBRoeg" bikes) ; gcn tech
         ;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCuTaETsuCOkJ0H_GAztWt0Q" bikes) ; gcn
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCUHW94eEFW7hkUMVaZz4eDg" physics interesting) ; minutephysics
+        ("https://www.youtube.com/feeds/videos.xml?channel_id=UCoxcjq-8xIDTYp3uz647V5A" interesting) ; numberphile
         ("https://www.youtube.com/feeds/videos.xml?channel_id=UCGamtiY212YK76rDI4IhCGg" bikes) ; cyclingabout
         ("https://www.youtube.com/feeds/videos.xml?channel_id=UCyG8fDB-wIkdjprZD-0TI6Q" bikes) ; bikepacking.com
         ("https://www.youtube.com/feeds/videos.xml?channel_id=UCNueufSUUGhQJngFPebQmzw" bikes) ; ted king
@@ -95,15 +91,16 @@
         ("http://feeds.feedburner.com/inrng/inrng0" bikes)
         ("http://feeds.feedburner.com/redkiteprayer/krin" bikes)
         ("https://bikepacking.com/feed/" bikes interesting)
-        ("https://blog.komoot.com/feed" blog)
         ("https://fahrradzukunft.de/feed/" bikes)
         ("https://www.iamtedking.com/blog?format=rss" bikes interesting)
         ("https://www.renehersecycles.com/feed/" bikes interesting)
+        ("https://blog.appliedcomputing.io/feed" blog)
 
                                         ; trying to keep taps on work stuff
         ("https://meltano.com/blog/feed/" data blog)
         ("https://roundup.getdbt.com/feed" data blog)
         ("https://www.ssp.sh/index.xml" data blog)
+        ("https://more-than-numbers.count.co/feed" blog)
         ))
 
 (setq-default elfeed-search-filter "@2-weeks-ago +unread ")
