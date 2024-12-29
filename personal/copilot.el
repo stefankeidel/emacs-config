0f745ac879563adbba6ae35fc25091223b67d62b
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2024-12-29 13:55:20 +0100

    copilot.el moved

--- personal/copilot.el
+++ personal/copilot.el
@@ -7,9 +7,13 @@
 
 (use-package copilot
   :quelpa (copilot :fetcher github
-                   :repo "zerolfx/copilot.el"
+                   :repo "copilot-emacs/copilot.el"
                    :branch "main"
-                   :files ("dist" "*.el")))
+                   :files ("dist" "*.el"))
+  :config
+  (add-to-list 'copilot-indentation-alist '(prog-mode 2))
+  (add-to-list 'copilot-indentation-alist '(python-ts-mode 2))
+)
 
 ;; you can utilize :map :hook and :config to customize copilot
 (add-hook 'prog-mode-hook 'copilot-mode)
