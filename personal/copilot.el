45d7c61c86ea93f86a7ab7674050ebaea581ab46
Stefan Keidel <1188614+stefankeidel@users.noreply.github.com>
2025-01-13 06:33:48 +0100

    let's see

--- personal/copilot.el
+++ personal/copilot.el
@@ -4,6 +4,9 @@
    :url "https://github.com/quelpa/quelpa-use-package.git"))
 (require 'quelpa-use-package)
 
+(use-package company-box
+  :ensure t
+  :hook (company-mode . company-box-mode))
 
 (use-package copilot
   :quelpa (copilot :fetcher github
@@ -11,10 +14,11 @@
                    :branch "main"
                    :files ("dist" "*.el"))
   :config
-  (add-to-list 'copilot-indentation-alist '(prog-mode 0))
-  (add-to-list 'copilot-indentation-alist '(python-ts-mode 0))
-  (add-to-list 'copilot-indentation-alist '(lisp-mode 2))
+  (setq copilot-indent-offset-warning-disable t)
+  (add-to-list 'copilot-indentation-alist '(python-ts-mode . 0))
+  (add-to-list 'copilot-indentation-alist '(python-mode . 0))
 )
+
 
 ;; you can utilize :map :hook and :config to customize copilot
 (add-hook 'prog-mode-hook 'copilot-mode)
